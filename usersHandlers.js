71b01f3d80ebe1084124cc3bf69ec5ed8e50a03c
Lumady <elodie.wsc@gmail.com>
2023-02-07 19:12:34 +0100

    bug resolved

--- usersHandlers.js
+++ usersHandlers.js
@@ -1,7 +1,8 @@
 const database = require("./database");
 /* quête 6 */
 const getUsers = (req, res) => {
-  const initialSql = "select * from users";
+  const initialSql =
+    "select firstname, lastname, email, city, language from users";
   const where = [];
 
   if (req.query.language != null) {
@@ -53,7 +54,10 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from users where id = ?", [id])
+    .query(
+      "select firstname, lastname, email, city, language from users where id = ?",
+      [id]
+    )
     .then(([users]) => {
       if (users[0] != null) {
         res.status(200).json(users[0]);
